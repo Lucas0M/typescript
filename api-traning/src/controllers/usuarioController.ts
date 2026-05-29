6e3f15635675604e243923d8cb35ecb48da6ba32
Lucas Macarini <contatolucasmacarini@gmail.com>
2026-05-29 00:57:41 -0300

    Add: Database (PostgreSQL) with Prisma

--- api-traning/src/controllers/usuarioController.ts
+++ api-traning/src/controllers/usuarioController.ts
@@ -1,36 +1,14 @@
 import { Request, Response } from "express";
+import prisma from "../prisma";
 
-type Usuario = {
-  id: number;
-  nome: string;
-  email: string;
-};
-
-const usuarios: Usuario[] = [
-  {
-    id: 1,
-    nome: "Lucas Ronaldo",
-    email: "lucas@gmail.com",
-  },
-  {
-    id: 2,
-    nome: "Joao Marcos",
-    email: "joao@gmail.com",
-  },
-  {
-    id: 3,
-    nome: "Pedro Silva",
-    email: "pedro@gmail.com",
-  },
-];
-
-export function listarUsuarios(req: Request, res: Response) {
+export async function listarUsuarios(req: Request, res: Response) {
+  const usuarios = await prisma.usuario.findMany();
   res.json(usuarios);
 }
 
-export function buscarUsuario(req: Request, res: Response) {
+export async function buscarUsuario(req: Request, res: Response) {
   const id: number = Number(req.params.id);
-  const usuario = usuarios.find((u) => u.id === id);
+  const usuario = await prisma.usuario.findUnique({ where: { id } });
 
   if (!usuario) {
     return res.status(404).json({ erro: "Erro user not found!" });
@@ -38,38 +16,36 @@ export function buscarUsuario(req: Request, res: Response) {
   res.json(usuario);
 }
 
-export function criarUsuario(req: Request, res: Response) {
-  const { nome, email }: Omit<Usuario, "id"> = req.body;
+export async function criarUsuario(req: Request, res: Response) {
+  const { nome, email } = req.body;
+
   if (!nome || !email) {
     return res.status(400).json({ erro: "Not enough data!" });
   }
-  const newUsuario: Usuario = { id: usuarios.length + 1, nome, email };
-  usuarios.push(newUsuario);
+
+  const newUsuario = await prisma.usuario.create({
+    data: { nome, email },
+  });
 
   res.status(201).json(newUsuario);
 }
 
-export function atualizarUsuario(req: Request, res: Response) {
+export async function atualizarUsuario(req: Request, res: Response) {
   const id: number = Number(req.params.id);
-  const { nome, email }: Omit<Usuario, "id"> = req.body;
-  const index = usuarios.findIndex((u) => u.id === id);
+  const { nome, email } = req.body;
 
-  if (index === -1) {
-    return res.status(404).json({ erro: "User not found!" });
-  }
+  const usuario = await prisma.usuario.update({
+    where: { id },
+    data: { nome, email },
+  });
 
-  usuarios[index] = { ...usuarios[index], nome, email };
-  res.json(usuarios[index]);
+  res.json(usuario);
 }
 
-export function deletarUsuario(req: Request, res: Response) {
+export async function deletarUsuario(req: Request, res: Response) {
   const id: number = Number(req.params.id);
-  const index: number = usuarios.findIndex((u) => u.id === id);
 
-  if (index === -1) {
-    return res.status(404).json({ erro: "User not found!" });
-  }
+  await prisma.usuario.delete({ where: { id } });
 
-  usuarios.splice(index, 1);
-  return res.json({ message: "User deleted sucessed!" });
+  res.json({ message: "User deleted sucessfully!" });
 }
